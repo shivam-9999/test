4935c4c4715e8c4d2ad6677ff6eab152c77c0d6b
shivam-9999 <shivammaniya7469@gmail.com>
2025-03-25 16:18:22 -0400

    Updated configuration for Azure deployment

--- demoproject/settings.py
+++ demoproject/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pd_$_)9kg@cqodu#+^bj7^uv4py=)4y9&h)j$he14ds&-7x6y6'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-your-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.azurewebsites.net', 'localhost', '127.0.0.1']
 
 
 # Application definition
--- demoproject/urls.py
+++ demoproject/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.views import hello_world
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('', hello_world, name='hello_world'),
 ]
